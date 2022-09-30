b0ae56c8f1edb79966f376132553f8d0546a7676
lishengjaw <79784988+lishengjaw@users.noreply.github.com>
2022-09-30 20:42:54 +0800

    Refactor code

--- Calculations.cpp
+++ Calculations.cpp
@@ -1,27 +1,9 @@
 #include <cmath>
 #include <iostream>
+#include "Calculations.h"
 #include "constants.h"
 
 using namespace std;
-
-class Calculations 
-{
-    public:
-        // Record
-        int GetMaxNumOfRecordsPerBlock(int blockSize);
-        int GetMaxNumOfRecordBlocks(int blockSize);
-        int GetMaxSizeOfRecordBlocks(int blockSize);
-
-        // Index
-        int GetMaxNumOfKeysPerIndexBlock(int blockSize);
-        int GetMinNumOfKeysPerInternalNode(int blockSize);
-        int GetMinNumOfKeysPerLeafNode(int blockSize);
-        int GetMaxHeightOfBPlusTree(int blockSize);
-        int GetMaxNumOfIndexBlocks(int blockSize);
-        int GetMaxSizeOfIndexBlocks(int blockSize);
-
-        int GetTotalBlockSize(int blockSize);
-};
 
 int Calculations::GetMaxNumOfRecordsPerBlock(int blockSize) 
 {
