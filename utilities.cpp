a898cdf277dac0f33e8a0baf2966664a9204b382
MIBN21 <92128168+MIBN21@users.noreply.github.com>
2022-10-01 11:17:53 +0800

    Add files via upload

--- utilities.cpp
+++ utilities.cpp
@@ -21,6 +21,7 @@ void store_records(Storage data);
 int main(){
     char sel1;
     int blockSize;
+    //Storage data = Storage(0,0);
     int dsSize; //disk storage size, in MB
     cout << "Allocating disk storage of size " << dsSize << "MB...\n";
     //code to allocate memory
@@ -78,6 +79,7 @@ int main(){
             switch (sel2)
             {
             case '0':
+            {
                 char sel3;
                 cout << "Change block size\n";
                 cout << "Please select block size to change to. (Enter 1 or 2)\n";
@@ -115,6 +117,7 @@ int main(){
                 // code to initialize database block size to 200B
                 cout << "Change complete. Block size has been set to " << blockSize << "B.\n";
                 break;
+            }
             case '1':
                 {
                     cout << "Run experiment 1\n";
@@ -125,34 +128,80 @@ int main(){
                     int reservation = 1000000;
                     Storage data = Storage(storage_size + reservation, blockSize);
                     store_records(data);
+                    cout << "okay";
                     break;
                 }
             case '2':
+            {
                 // code to run experiment 2
                 cout << "Run experiment 2\n";
                 break;
+            }
             case '3':
+            {
                 // code to run experiment 3
                 cout << "Run experiment 3\n";
+
+                //Data block portion fo teh code
+                int block_count = 0;
+                int record_count = 0;
+                float average_rating = 0;
+                char* prev_block_address = 0;
+                //while list of record addresses is not empty
+                // {
+                //     char* current_block_address = data.record_addr_to_blk_addr(char* rarr)
+                //     if (current_block_address == prev_block_address)
+                //     {       
+                //         //retrieve the avg rating of the individual record
+                //             Record temp_record = data.retrieve_record(char*rarr)
+                //             average_rating += temp_record.rating2Dec()
+                //             record_count += 1
+                //     }
+                //     else{
+                //         if(block_count<5)
+                //         {
+                //             vector<Record> contents_of_block = data.retrieve_blk(current_block_address)
+                //             //print of the contents of the block using while/for loop
+                //         }
+                //         Record temp_record = data.retrieve_record(char*rarr)
+                //         average_rating += temp_record.rating2Dec()
+                //         // increment record count no matter what, but block count only if its not consecutively the same
+                //         record_count += 1
+                //         block_count +=1
+                //     }
+                //     //change the prev to current for next loop
+                //     prev_block_address = current_block_address
+                // }
+                cout<< "Number of blocks accessed is " << block_count;
+                //the contents of first 5 printed out beforehand in the if portion
+                cout << "Average rating of all the recors accesed is " << average_rating/record_count;
+
                 break;
+            }
             case '4':
+            {
                 // code to run experiment 4
                 cout << "Run experiment 4\n";
                 break;
+            }
             case '5':
-                // code to run experiment 5
+             {   // code to run experiment 5
                 cout << "Run experiment 5\n";
                 break;
+             }
             case '6':
+            {
                 cout << "Terminating program....\n";
                 // clean up code if any
                 cout << "Bye!";
                 break;
+            }
             default:
+            {
                 validSelection = false;
                 cout << "Your selection is invalid, please select a valid option. (0, 1, 2, 3, 4, 5, or, 6)\n";
             }
-
+            }
         } while (!validSelection);
     }
 }
@@ -164,15 +213,16 @@ void store_records(Storage data) {
     ifstream testfile("data.tsv");
     string testline;
     // this commeneted out block is to find out number of records im working on a dynamic one but this one works fine like so it works but not best implementation
-    int numofrecords= -1;
-    while (getline(testfile, testline))
-    {
-        numofrecords+=1;
-    }
+    // int numofrecords= -1;
+    // while (getline(testfile, testline))
+    // {
+    //     numofrecords+=1;
+    // }
     // cout<< numofrecords;
     //actual number of record is 1070318
     int skip_header = 0;
-    int record_number = 0;
+    //for testing purposes
+    int record_number =0;
     // cout<< "ok";
     ifstream infile("data.tsv");
     string line;
@@ -215,8 +265,11 @@ void store_records(Storage data) {
 
         data.insert_item(&temp, RECORD_SIZE);
         cout << "\n";
-        //move on to next record by incrementing array pointer
         record_number++;
+        if (record_number>100)
+        {
+            break;
+        }
     }
     infile.close();
 
@@ -226,5 +279,6 @@ void store_records(Storage data) {
     cout << "Record size: " << RECORD_SIZE << " Btyes" <<endl;
     cout << "Allocated number of blocks: " << data.get_allocated_nof_blk() << endl;
     cout << "Allocated size: " << data.allocated_size<<endl;
+    return;
 }
 
